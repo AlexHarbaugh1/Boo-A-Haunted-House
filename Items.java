2fdeb6f01cccef69f5d5cb91db46af1a07b8fe6d
AlexHarbaugh1 <amh0053@uah.edu>
2025-03-13 11:33:12 -0500

    Create Items.java

---
+++ Items.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hyatt.branch;
+import hyatt.branch.Player;
+/**
+ *
+ * @author hyattkamel
+ */
+public class Items {
+        // Weapons
+        public final double FIST; 
+        public final double STAKE;
+        public final double CLUB; 
+        public final double KNIFE; 
+        public final double WHIP;
+        public final double GUN;
+
+        // Key
+        public final boolean KEY;
+ 
+
+    // Constructor for Items bag(?)
+    
+    public Items() {
+        // WEAPONS
+        FIST = 0;
+        STAKE = 10^1;
+        CLUB = 10^2;
+        KNIFE = 10^3;
+        WHIP = 10^4;
+        GUN = 10^5;
+        // KEY
+        KEY = true;
+        
+        //HEALTH POTIONS (enhancement)
+        
+    }
+    
+     
+}
